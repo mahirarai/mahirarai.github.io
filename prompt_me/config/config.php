7759898276dbfcaa15d445da20c32f9a5c91466c
mahira <42784156+mahirarai@users.noreply.github.com>
2020-07-12 12:04:06 -0400

    Revert "Prompt Me"
    This reverts commit 4a70d855e787916912b43989a3a1ab5fd6c80589.

--- prompt_me/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-define('DB_HOST', '303.itpwebdev.com');
-define('DB_USER', 'raihan_db_user');
-define('DB_PASS', 'Itpwebdev!');
-define('DB_NAME', 'raihan_final_db');
-
-?>
